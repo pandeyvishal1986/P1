23a2582e611102f58c3d1cd5f45cdba57b93e177
vishalpandey <vishal.pandey@nagarro.com>
2018-08-14 12:05:52 +0530

    changes in develoop

--- CheckUnity/SchoolDBContext.cs
+++ CheckUnity/SchoolDBContext.cs
@@ -33,6 +33,8 @@ namespace SchoolDataLayer
             defaultStandards.Add(new Student() { StudentName = "Bill1", Weight = 100 });
             defaultStandards.Add(new Student() { StudentName = "Bill2", Weight = 100 });
             defaultStandards.Add(new Student() { StudentName = "Bill3", Weight = 100 });
+            defaultStandards.Add(new Student() { StudentName = "Bill4", Weight = 78 });
+            defaultStandards.Add(new Student() { StudentName = "Bill5", Weight = 90 });
 
             context.Students.AddRange(defaultStandards);
             base.Seed(context);
